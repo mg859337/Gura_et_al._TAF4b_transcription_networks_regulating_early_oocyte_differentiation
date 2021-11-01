542effd39ffb86020534ac95868ea285a604bb59
mg859337 <48734633+mg859337@users.noreply.github.com>
2021-11-01 14:29:00 -0400

    Update Homer_MakeTags.sh

--- CUT&RUN_data_analysis/Homer_MakeTags.sh
+++ CUT&RUN_data_analysis/Homer_MakeTags.sh
@@ -17,9 +17,13 @@ module load samtools
 
 # make the tag directories for homer
 
-makeTagDirectory F-TAF4b_1.120/ F-TAF4b.1_120.bam
+makeTagDirectory F-TAF4b_1.120/ F-TAF4b_1.1_120.bam
+makeTagDirectory F-TAF4b_2.120/ F-TAF4b_2.1_120.bam
 
-makeTagDirectory F-H3K4_150.500/ F-H3K4.150_500.bam
-makeTagDirectory F-IgG_1.120/ F-IgG.1_120.bam
-makeTagDirectory F-IgG_150.500/ F-IgG.150_500.bam
+makeTagDirectory F-H3K4_1_150.500/ F-H3K4_1.150_500.bam
+makeTagDirectory F-H3K4_2_150.500/ F-H3K4_2.150_500.bam
 
+makeTagDirectory F-IgG_1_1.120/ F-IgG_1.1_120.bam
+makeTagDirectory F-IgG_2_1.120/ F-IgG_2.1_120.bam
+makeTagDirectory F-IgG_1_150.500/ F-IgG_1.150_500.bam
+makeTagDirectory F-IgG_2_150.500/ F-IgG_2.150_500.bam
